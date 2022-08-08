ba024f6c86fe268eb2242739d158c7a9c4a61969
Pedro de Matos Fedricci <pedromfedricci@gmail.com>
2022-08-08 04:42:19 -0300

    Change name convention on deserialize

--- src/deserialize/macros.rs
+++ src/deserialize/macros.rs
@@ -1,25 +1,25 @@
 macro_rules! constrained_deserialize_impl {
-    ($Int:ty, $int_md:ident, $Ty:ident, $deserialize:ident, $($method:ident!($Inner:ty, $($Arg:ty : $visit:ident)*);)*) => {
+    ($Num:ty, $num_mod:ident, $Cnst:ident, $deserialize:ident, $($method:ident!($Inner:ty, $($Visit:ty : $visit:ident)*);)*) => {
         #[cfg(feature = "serde")]
-        impl<'de, const MIN: $Int, const MAX: $Int, const DEF: $Int> ::serde::Deserialize<'de>
-            for crate::$int_md::$Ty<MIN, MAX, DEF>
+        impl<'de, const MIN: $Num, const MAX: $Num, const DEF: $Num> ::serde::Deserialize<'de>
+            for crate::$num_mod::$Cnst<MIN, MAX, DEF>
         {
             fn deserialize<D: ::serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
                 use ::serde::de::{Error as DesError, Visitor, Unexpected};
                 use ::core::fmt::{Formatter, Result as FmtResult};
-                // use crate::$int_md::guard_construction;
+                // use crate::$num_mod::guard_construction;
 
-                struct ConstrainedVisitor<const MIN: $Int, const MAX: $Int, const DEF: $Int>;
+                struct ConstrainedVisitor<const MIN: $Num, const MAX: $Num, const DEF: $Num>;
 
                 // TODO: reuse modules's guard instead.
                 // Unfortunate workaround.
                 // Issue: https://github.com/Mari-W/const_guards/issues/2.
                 #[inline(always)]
-                const fn guard_construction<const MIN: $Int, const MAX: $Int, const DEF: $Int>() -> bool {
-                    (MIN < MAX) && (DEF >= MIN && DEF <= MAX)  && (MIN > <$Int>::MIN || MAX < <$Int>::MAX)
+                const fn guard_construction<const MIN: $Num, const MAX: $Num, const DEF: $Num>() -> bool {
+                    (MIN < MAX) && (DEF >= MIN && DEF <= MAX)  && (MIN > <$Num>::MIN || MAX < <$Num>::MAX)
                 }
 
-                impl<const MIN: $Int, const MAX: $Int, const DEF: $Int> ConstrainedVisitor<MIN, MAX, DEF> {
+                impl<const MIN: $Num, const MAX: $Num, const DEF: $Num> ConstrainedVisitor<MIN, MAX, DEF> {
                     fn guard_construction<E: DesError>(&self) -> Result<(), E> {
                         if guard_construction::<MIN, MAX, DEF>() {
                             Ok(())
@@ -29,16 +29,16 @@ macro_rules! constrained_deserialize_impl {
                     }
                 }
 
-                impl<const MIN: $Int, const MAX: $Int, const DEF: $Int> Visitor<'_>
+                impl<const MIN: $Num, const MAX: $Num, const DEF: $Num> Visitor<'_>
                     for ConstrainedVisitor<MIN, MAX, DEF>
                 {
-                    type Value = crate::$int_md::$Ty<MIN, MAX, DEF>;
+                    type Value = crate::$num_mod::$Cnst<MIN, MAX, DEF>;
 
                     fn expecting(&self, f: &mut Formatter<'_>) -> FmtResult {
-                        write!(f, "a constrained {} value within {MIN}..={MAX}", stringify!($Int))
+                        write!(f, "a constrained {} value within {MIN}..={MAX}", stringify!($Num))
                     }
 
-                    $($($method!($Inner, $Arg : $visit);)*)*
+                    $($($method!($Inner, $Visit : $visit);)*)*
                 }
 
                 deserializer.$deserialize(ConstrainedVisitor)
@@ -49,10 +49,10 @@ macro_rules! constrained_deserialize_impl {
 
 // Equivalent to serde's `num_self!` and `num_as_self!` but for uint only.
 macro_rules! num_as_self_uint {
-    ($UnsInt:ty, $Arg:ty : $visit:ident) => {
-        fn $visit<E: DesError>(self, v: $Arg) -> Result<Self::Value, E> {
+    ($UnsInner:ty, $UnsInt:ty : $visit:ident) => {
+        fn $visit<E: DesError>(self, v: $UnsInt) -> Result<Self::Value, E> {
             self.guard_construction()?;
-            Self::Value::__new(v as $UnsInt)
+            Self::Value::__new(v as $UnsInner)
                 .map_err(|_| E::invalid_value(Unexpected::Unsigned(v as u64), &self))
         }
     };
@@ -60,22 +60,22 @@ macro_rules! num_as_self_uint {
 
 // Equivalent to serde's `num_self!` and `num_as_self!` but for sint only.
 macro_rules! num_as_self_int {
-    ($SigInt:ty, $Arg:ty : $visit:ident) => {
-        fn $visit<E: DesError>(self, v: $Arg) -> Result<Self::Value, E> {
+    ($SigInner:ty, $SigInt:ty : $visit:ident) => {
+        fn $visit<E: DesError>(self, v: $SigInt) -> Result<Self::Value, E> {
             self.guard_construction()?;
-            Self::Value::__new(v as $SigInt)
+            Self::Value::__new(v as $SigInner)
                 .map_err(|_| E::invalid_value(Unexpected::Signed(v as i64), &self))
         }
     };
 }
 
 macro_rules! uint_to_self {
-    ($UnsInt:ty, $Arg:ty : $visit:ident) => {
-        fn $visit<E: DesError>(self, v: $Arg) -> Result<Self::Value, E> {
+    ($Inner:ty, $UnsInt:ty : $visit:ident) => {
+        fn $visit<E: DesError>(self, v: $UnsInt) -> Result<Self::Value, E> {
             self.guard_construction()?;
 
-            if v as u64 <= <$UnsInt>::MAX as u64 {
-                if let Ok(value) = Self::Value::__new(v as $UnsInt) {
+            if v as u64 <= <$Inner>::MAX as u64 {
+                if let Ok(value) = Self::Value::__new(v as $Inner) {
                     return Ok(value);
                 }
             }
@@ -85,12 +85,12 @@ macro_rules! uint_to_self {
 }
 
 macro_rules! int_to_int {
-    ($SigInt:ty, $Arg:ty : $visit:ident) => {
-        fn $visit<E: DesError>(self, v: $Arg) -> Result<Self::Value, E> {
+    ($SigInner:ty, $SigInt:ty : $visit:ident) => {
+        fn $visit<E: DesError>(self, v: $SigInt) -> Result<Self::Value, E> {
             self.guard_construction()?;
 
-            if <$SigInt>::MIN as i64 <= v as i64 && v as i64 <= <$SigInt>::MAX as i64 {
-                if let Ok(value) = Self::Value::__new(v as $SigInt) {
+            if <$SigInner>::MIN as i64 <= v as i64 && v as i64 <= <$SigInner>::MAX as i64 {
+                if let Ok(value) = Self::Value::__new(v as $SigInner) {
                     return Ok(value);
                 }
             }
@@ -100,16 +100,16 @@ macro_rules! int_to_int {
 }
 
 macro_rules! int_to_uint {
-    ($UnsInt:ty, $Arg:ty : $visit:ident) => {
-        fn $visit<E: DesError>(self, v: $Arg) -> Result<Self::Value, E> {
+    ($UnsInner:ty, $SigInt:ty : $visit:ident) => {
+        fn $visit<E: DesError>(self, v: $SigInt) -> Result<Self::Value, E> {
             self.guard_construction()?;
 
-            if 0 < v && v as u64 <= <$UnsInt>::MAX as u64 {
-                Self::Value::__new(v as $UnsInt)
-                    .map_err(|_| E::invalid_value(Unexpected::Unsigned(v as u64), &self))
-            } else {
-                Err(E::invalid_value(Unexpected::Signed(v as i64), &self))
+            if 0 < v && v as u64 <= <$UnsInner>::MAX as u64 {
+                if let Ok(value) = Self::Value::__new(v as $UnsInner) {
+                    return Ok(value);
+                }
             }
+            Err(E::invalid_value(Unexpected::Signed(v as i64), &self))
         }
     };
 }
