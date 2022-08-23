a3e520345ae7ed8b28eb2fb82007a21e41a0e6cd
Pedro de Matos Fedricci <pedromfedricci@gmail.com>
2022-08-23 16:11:41 -0300

    Fix public API report script

--- ci/report_public_api.sh
+++ ci/report_public_api.sh
@@ -3,12 +3,13 @@
 export LATEST_TAG_COMMIT=$(git ls-remote --sort='-version:refname' --tags origin | head -n 1 | awk '{print $1}')
 export CURRENT_HEAD=$(git rev-parse HEAD)
 
-# Silence the `detached state` message, unimportant. 
-git config advice.detachedHead false
+# Print current version.
+cargo public-api --version
 # Report public API changes in comparison to the latest tag at origin.
-cargo public-api --diff-git-checkouts $LATEST_TAG_COMMIT $CURRENT_HEAD
-# Just move back to whatever checkout we were before.
-git checkout --quiet @{-2}
+cargo public-api \
+    --rustdoc-json-toolchain +$1 \
+    --diff-git-checkouts $LATEST_TAG_COMMIT $CURRENT_HEAD
+
 # This is just a report script, don't fail a job run just because
 # something went wrong.
 exit 0
