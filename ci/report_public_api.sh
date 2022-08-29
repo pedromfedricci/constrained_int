80b2a87346daeea1bd9ae5747ddc8ba36a9f5fff
Pedro de Matos Fedricci <pedromfedricci@gmail.com>
2022-08-29 01:23:17 -0300

    Update public api script

--- ci/report_public_api.sh
+++ ci/report_public_api.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env sh
 
+if [ -z "$1" ]; then
+    export TOOLCHAIN=""
+else
+    export TOOLCHAIN="+$1"
+fi
+
 export LATEST_TAG_COMMIT=$(git ls-remote --sort='-version:refname' --tags origin | head -n 1 | awk '{print $1}')
 export CURRENT_HEAD=$(git rev-parse HEAD)
+export CHECKOUT=$(git symbolic-ref --short HEAD)
 
 # Print current version.
 cargo public-api --version
 # Report public API changes in comparison to the latest tag at origin.
-cargo public-api \
-    --rustdoc-json-toolchain +$1 \
-    --diff-git-checkouts $LATEST_TAG_COMMIT $CURRENT_HEAD
+cargo $TOOLCHAIN public-api --diff-git-checkouts $LATEST_TAG_COMMIT $CURRENT_HEAD
 
-# This is just a report script, don't fail a job run just because
-# something went wrong.
+if [ $? -ne 0 ]; then
+    # Move back to whatever checkout we were before.
+    git checkout --quiet $CHECKOUT
+fi
+
+# This script must not fail a job run.
 exit 0
